d22eaea717a2b2cc0ca5a69c569bc1a43fa4e9ca
davidlb365 <davidlb365@gmail.com>
2022-12-29 23:27:23 +0100

    fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config()
 
 conectarDB()
 
-const dominiosPermitidos = [process.env.FRONTEND_URL]
+const dominiosPermitidos = [process.env.FRONTEND_UR]
 
 const corsOptions = {
     origin: function(origin, callback) {
