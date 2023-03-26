c7fff3bb8f99743cd565a62a82479921b5b8528a
davidlb365 <davidlb365@gmail.com>
2023-03-26 22:12:45 +0200

    commit

--- index.js
+++ index.js
@@ -12,21 +12,22 @@ dotenv.config()
 
 conectarDB()
 
-const dominiosPermitidos = [process.env.FRONTEND_URL, process.env.SECOND_URL]
+// const dominiosPermitidos = [process.env.FRONTEND_URL, process.env.SECOND_URL]
 
-const corsOptions = {
-    origin: function(origin, callback) {
-        if(dominiosPermitidos.indexOf(origin) !== -1) {
-            // El origen del Request está permitido
-            callback(null, true)
-        }
-        else {
-            callback(new Error('No permitido por CORS'))
-        }
-    }
-}
+// const corsOptions = {
+//     origin: function(origin, callback) {
+//         if(dominiosPermitidos.indexOf(origin) !== -1) {
+//             // El origen del Request está permitido
+//             callback(null, true)
+//         }
+//         else {
+//             callback(new Error('No permitido por CORS'))
+//         }
+//     }
+// }
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+app.use(cors())
 
 app.use('/api/veterinarios', veterinarioRoutes)
 app.use('/api/pacientes', pacienteRoutes)
