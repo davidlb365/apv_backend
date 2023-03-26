1f3268300a63dc0d6928db35fb5f97447673d88c
davidlb365 <davidlb365@gmail.com>
2023-03-26 23:25:17 +0200

    commit

--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -13,7 +13,7 @@ const emailRegistro = async (datos) => {
     const {email, nombre, token} = datos
     // Enviar el email
     const info = await transporter.sendMail({
-        from: "APV - Administrador de Pacientes de Veterinaria",
+        from: '"APV - Administrador de Pacientes de Veterinaria" <apv@correo.com>',
         to: email,
         subject: 'Comprueba tu cuenta en APV',
         text: 'Comprueba tu cuenta en APV',
